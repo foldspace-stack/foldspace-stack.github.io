d3e0fdc61739155dcff27098b37b1fd4f6289905
timger <yishenggudou@gmail.com>
2025-03-28 16:55:05 +0800

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`@${siteConfig.title}`}
+      description={`${siteConfig.title} 马栏山最好的AI服务提供商`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
