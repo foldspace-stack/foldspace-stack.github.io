b5e6630b1b08a3c5f194c86d455876f7e05b365b
timger <yishenggudou@gmail.com>
2025-03-28 17:42:08 +0800

    del

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,58 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "AI私有化部署解决方案",
+    Svg: require("./svgs/item1.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <p>√ 自主可控的垂直领域大模型</p>
+        <p>√ 企业级知识库智能重构</p>
+        <p>√ 私有云+混合云双轨部署</p>
+        <p>√ GDPR/等保三级合规架构</p>
+        <p>"让每个企业拥有专属的AI智慧中枢"</p>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "智能ERP&软件定制",
+    Svg: require("./svgs/item2.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <p>√ 行业化深度定制（制造/零售/医疗专属方案）</p>
+        <p>√ 业务流程数字孪生建模</p>
+        <p>√ BI智能决策中台</p>
+        <p>√ 供应链金融协同系统</p>
+        <p>"深度定制贴身服务,成本优势"</p>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "MCN全域IP孵化",
+    Svg: require("./svgs/item3.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <p>√ 达人矩阵生态构建 </p>
+        <p>√ 短视频工业化生产体系</p>
+        <p>√ 品牌IP元宇宙升级</p>
+        <p>√ 电商全链路变现设计</p>
+        <p>"从流量到留量的内容商业新基建"</p>
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
